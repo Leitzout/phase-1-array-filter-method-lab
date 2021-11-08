ac641ad59dbb1955eec169679d20b5194070d06d
Kaylee Marie Leitzel <leitz@Leitzout.localdomain>
2021-11-08 16:28:14 -0700

    commit tree 86011a427f6aa94b4a79063d6f0db28431367d49

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching (drivers, string) {
+   let filteredArray = drivers.filter(name => name === string || name === string.toLowerCase());
+   console.log(filteredArray)
+   return filteredArray;
+}
+
+function fuzzyMatch (drivers, string) {
+   const saMatch = drivers.filter(driver => driver.slice(0,string.length) == string)
+   return saMatch;
+}
+
+function matchName (drivers, string) {
+   const findBobby = drivers.filter(driver => driver.name ==="Bobby")
+   return findBobby;
+}
